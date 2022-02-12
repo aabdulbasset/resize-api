34e11135c98cc0aca6ee5cd399fd3e6c46d95ebe
Ahmed basset <aabdulbasset5@gmail.com>
2022-02-12 23:53:30 +0200

    Added readme

---
+++ README.md
@@ -0,0 +1,27 @@
+
+# resize-api
+
+A simple image resizing api made with express and nodejs
+
+
+## Guide
+
+To Start this project run
+
+```bash
+  npm run start
+```
+
+
+## Running Tests
+
+For Jasmine tests 
+
+```bash
+  npm run test
+```
+
+For Eslint with prettier
+```bash
+  npm run lint
+```
--- server/resize.js
+++ server/resize.js
@@ -1,12 +1,19 @@
-"use strict";
-var __importDefault = (this && this.__importDefault) || function (mod) {
-    return (mod && mod.__esModule) ? mod : { "default": mod };
-};
-Object.defineProperty(exports, "__esModule", { value: true });
-var sharp_1 = __importDefault(require("sharp"));
+'use strict';
+var __importDefault =
+  (this && this.__importDefault) ||
+  function (mod) {
+    return mod && mod.__esModule ? mod : { default: mod };
+  };
+Object.defineProperty(exports, '__esModule', { value: true });
+var sharp_1 = __importDefault(require('sharp'));
 function main(fileName, dimensions) {
-    (0, sharp_1.default)("../images/full/".concat(fileName, ".jpg"))
-        .resize(dimensions[0], dimensions[1])
-        .toFile("../images/thumb/".concat(fileName, "_").concat(dimensions[0], "x").concat(dimensions[1], ".jpg"));
+  (0, sharp_1.default)('../images/full/'.concat(fileName, '.jpg'))
+    .resize(dimensions[0], dimensions[1])
+    .toFile(
+      '../images/thumb/'
+        .concat(fileName, '_')
+        .concat(dimensions[0], 'x')
+        .concat(dimensions[1], '.jpg')
+    );
 }
 exports.default = main;
